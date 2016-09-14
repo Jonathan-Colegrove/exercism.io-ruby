646f152162fa807bfa46e19aa498a873e32a1818
Jonathan Colegrove <Demetrius472@gmail.com>
2016-09-14 15:28:40 -0400

    finished Hamming

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -1,7 +1,18 @@
+module BookKeeping
+  VERSION = 3
+end
+
 class Hamming
   def self.compute(first_strand, second_strand)
-    if first_strand == second_strand
-      return 0
+    a = first_strand.chars
+    b = second_strand.chars
+
+    raise ArgumentError unless a.size == b.size
+
+    differences = 0
+    while a.size > 0
+      a.shift == b.shift ? next : differences += 1
     end
+    differences
   end
 end
