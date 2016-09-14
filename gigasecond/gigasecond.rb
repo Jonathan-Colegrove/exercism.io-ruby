70c2be22f705de1e6f9600aab48b1949a54e6ebd
Jonathan Colegrove <Demetrius472@gmail.com>
2016-09-14 17:47:25 -0400

    finished gigasecond

--- gigasecond/gigasecond.rb
+++ gigasecond/gigasecond.rb
@@ -1,9 +1,8 @@
 class Gigasecond
-  def self.from(time)
-    return Time.utc(2043, 1, 1, 1, 46, 40)
+  def self.from(birth)
+    birth + 1_000_000_000
   end
 end
-
 
 module BookKeeping
   VERSION = 4
