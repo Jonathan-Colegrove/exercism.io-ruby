e0903c9889d8b0ea0225024acc857886c0005c99
Jonathan Colegrove <Demetrius472@gmail.com>
2016-09-15 17:34:47 -0400

    works if the 1st number in phone_numbers is a 1

--- phone-number/phone_number.rb
+++ phone-number/phone_number.rb
@@ -7,7 +7,9 @@ class PhoneNumber
 
   def number
     r = @n.gsub(/[^0-9]/, '')
-    if r.size == 10
+    if r.size == 11 && r[0] == "1"
+      return r[1..-1]
+    elsif r.size == 10
       return r
     else
       return '0000000000'
