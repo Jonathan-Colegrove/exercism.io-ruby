9c6690253b3e9c1c8c01c11d345ad9125dc469e0
Jonathan Colegrove <Demetrius472@gmail.com>
2016-09-15 19:32:06 -0400

    phone works amazing woo

--- phone-number/phone_number.rb
+++ phone-number/phone_number.rb
@@ -6,15 +6,17 @@ class PhoneNumber
   end
 
   def number
+    # If n contains letters
     if n =~ /[a-z]/
       return '0000000000'
     end
 
-    @r = @n.gsub(/[^0-9]/, '')
-    if @r.size == 11 && @r[0] == "1"
-      return @r[1..-1]
-    elsif @r.size == 10
-      return @r
+    r = @n.gsub(/[^0-9]/, '')
+
+    if r.size == 11 && r[0] == "1"
+      return r[1..-1]
+    elsif r.size == 10
+      return r
     else
       return '0000000000'
     end
@@ -22,6 +24,18 @@ class PhoneNumber
 
   def area_code
     return @n.gsub(/[^0-9]/, '')[0..2]
+  end
+
+  def to_s
+    r = @n.gsub(/[^0-9]/, '')
+    if r.size == 11 && r[0] == "1"
+      a = r[1..-1]
+      return "(#{a[0..2]}) #{a[3..5]}-#{a[6..9]}"
+    elsif r.size == 10
+      return "(#{r[0..2]}) #{r[3..5]}-#{r[6..9]}"
+    else
+      return '0000000000'
+    end
   end
 
 end
